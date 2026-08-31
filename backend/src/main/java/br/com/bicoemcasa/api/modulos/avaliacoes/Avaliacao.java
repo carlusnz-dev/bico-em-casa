e93a6773c8319e6ff22c8f4765898ba423f609e9
Thiago-LimaS <tlimasantos2013@gmail.com>
2026-08-31 20:46:26 -0300

    feat: add entity and repository avaliacoes

--- backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/Avaliacao.java
+++ backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/Avaliacao.java
@@ -1,4 +1,52 @@
 package br.com.bicoemcasa.api.modulos.avaliacoes;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name= "avaliacao")
+@Getter
+@Setter
+
 public class Avaliacao {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "contratacao_id", unique = true, nullable = false)
+    private UUID contratacaoId;
+
+    @Column(name = "autor_id", nullable = false)
+    private Long autorId;
+
+    @Column(name = "avaliado_id", nullable = false)
+    private Long avaliadoId;
+
+    @Column(nullable = false)
+    private Integer nota;
+
+    @Column(length = 1000)
+    private String comentario;
+
+    @Column(name = "criado_em", nullable = false)
+    private OffsetDateTime criadoEm;
+
+    @Column(name = "atualizado_em", nullable = false)
+    private OffsetDateTime atualizadoEm;
+
+
+    @PrePersist
+    protected void criar() {
+        this.criadoEm = OffsetDateTime.now();
+        this.atualizadoEm = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    protected void atualizar() {
+        this.atualizadoEm = OffsetDateTime.now();
+    }
+
 }
--- backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/AvaliacaoRepository.java
+++ backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/AvaliacaoRepository.java
@@ -1,4 +1,20 @@
 package br.com.bicoemcasa.api.modulos.avaliacoes;
 
-public interface AvaliacaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository <Avaliacao , UUID> {
+
+
+    Optional<Avaliacao> findByContratacaoId(@Param("contratacaoId")UUID contratacaoId);
+    List<Avaliacao> findByAvaliadoId (Long avaliadoId);;
+
+    boolean existsByContratacaoId(UUID contratacaoId);
+
 }
