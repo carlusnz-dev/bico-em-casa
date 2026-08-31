f7b34de4216f1b0bc869cead04b50094d8912f92
Thiago-LimaS <tlimasantos2013@gmail.com>
2026-08-31 18:59:28 -0300

    add package avaliacoes

---
+++ backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/AvaliacaoRepository.java
@@ -0,0 +1,4 @@
+package br.com.bicoemcasa.api.modulos.avaliacoes;
+
+public interface AvaliacaoRepository {
+}
---
+++ backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/dto/AvaliacaoRequest.java
@@ -0,0 +1,4 @@
+package br.com.bicoemcasa.api.modulos.avaliacoes.dto;
+
+public record AvaliacaoRequest() {
+}
