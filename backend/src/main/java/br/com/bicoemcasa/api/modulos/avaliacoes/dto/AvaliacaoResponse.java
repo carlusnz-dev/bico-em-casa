e78d155f8cd8af6164c67d31997e3cd8be73ca93
Thiago-LimaS <tlimasantos2013@gmail.com>
2026-09-02 21:13:29 -0300

    feat: add dto: request and response

--- backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/dto/AvaliacaoResponse.java
+++ backend/src/main/java/br/com/bicoemcasa/api/modulos/avaliacoes/dto/AvaliacaoResponse.java
@@ -1,4 +1,16 @@
 package br.com.bicoemcasa.api.modulos.avaliacoes.dto;
 
-public record AvaliacaoResponse() {
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record AvaliacaoResponse(
+        UUID id,
+        UUID contratacaoId,
+        Long autorId,
+        Long avaliadoId,
+        Integer nota,
+        String comentario,
+        OffsetDateTime criadoEm,
+        OffsetDateTime atualizadoEm
+) {
 }
